5b29dd928f1fc56cd02fc3d6193a2aac241ada82
mohamed kamal <kmido9729@gmail.com>
2024-06-21 23:55:57 +0200

    rediss

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,57 +1,80 @@
 #!/usr/bin/env python3
-import redis
+""" Cache module handling the caching of an application"""
 import uuid
-from typing import Union, Callable, Optional
+from typing import Union, Optional, Callable
+from functools import wraps
+import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Count calls decorator """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ Call history decorator """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        key = method.__qualname__
+        input = str(args)
+        self._redis.rpush(f"{key}:inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(f"{key}:outputs", output)
+        return output
+
+    return wrapper
+
+
+def replay(method: Callable):
+    """ Replay decorator print history of calls of a particular function """
+    r = redis.Redis()
+    key = method.__qualname__
+    count = r.get(key)
+    if not count:
+        count = 0
+    inputs = r.lrange(f"{key}:inputs", 0, -1)
+    outputs = r.lrange(f"{key}:outputs", 0, -1)
+    print(f"{key} was called {count} times:")
+    for i, o in zip(inputs, outputs):
+        print(f"{key}(*{i.decode('utf-8')}) -> {o.decode('utf-8')}")
 
 
 class Cache:
-    """Cache class for Redis"""
+    """ Cache class handles caching using redis """
 
     def __init__(self):
-        """Initialize the cache"""
+        """ init a Cache object """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store the input data in Redis using a random key.
+    @count_calls
+    @call_history
+    def store(self, data: Union[bytes, str, int, float]) -> str:
+        """ Store data in redis """
+        r = str(uuid.uuid4())
+        self._redis.set(r, data)
+        return r
 
-        :param data: The data to store, which can be a string, bytes, integer, or float.
-        :return: The generated random key as a string.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
-
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
-        """
-        Retrieve data from Redis and convert it back to the desired format using the callable fn.
-
-        :param key: The key string to retrieve from Redis.
-        :param fn: An optional callable used to convert the data.
-        :return: The retrieved data, possibly converted using fn, or None if the key does not exist.
-        """
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[bytes, str, int, float]:
+        """ Get data from redis """
         data = self._redis.get(key)
-        if data is None:
-            return None
-        if fn is not None:
-            return fn(data)
+        if fn:
+            data = fn(data)
         return data
 
-    def get_str(self, key: str) -> Optional[str]:
-        """
-        Retrieve a string from Redis.
-
-        :param key: The key string to retrieve from Redis.
-        :return: The retrieved string or None if the key does not exist.
-        """
+    def get_str(self, key: str) -> str:
+        """ Get string from redis """
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
-    def get_int(self, key: str) -> Optional[int]:
-        """
-        Retrieve an integer from Redis.
-
-        :param key: The key string to retrieve from Redis.
-        :return: The retrieved integer or None if the key does not exist.
-        """
+    def get_int(self, key: str) -> int:
+        """ Get int from redis """
         return self.get(key, fn=int)
